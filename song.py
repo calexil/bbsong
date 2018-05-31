239ae3cd7cc93dcc87eeb12af8c5da9dc4072311
JD <calexil@users.noreply.github.com>
2018-05-31 00:37:25 -0400

    initial commit

---
+++ song.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+
+import pyautogui
+import os
+# Set a counter to count the # of exceptions occur
+counter = 0
+
+# Start the while loop
+while True:
+    try:
+        pyautogui.time.sleep(2)
+# Air combo starter
+        pyautogui.keyDown('i')
+        pyautogui.time.sleep(1)
+        pyautogui.keyUp('i')
+        pyautogui.keyDown('w')
+        pyautogui.keyDown('j')
+        pyautogui.keyUp('j')
+        pyautogui.keyDown('k')
+        pyautogui.keyUp('k')
+        pyautogui.keyDown('l')
+        pyautogui.keyUp('l')
+        pyautogui.keyDown('u')
+        pyautogui.keyUp('u')
+        pyautogui.keyDown('i')
+        pyautogui.keyUp('i')
+        pyautogui.keyDown('o')
+        pyautogui.keyUp('o')
+
+        pyautogui.keyUp('w')
+
+        pyautogui.keyDown('j')
+        pyautogui.keyUp('j')
+        pyautogui.keyDown('k')
+        pyautogui.keyUp('k')
+        pyautogui.keyDown('l')
+        pyautogui.keyUp('l')
+        pyautogui.keyDown('u')
+        pyautogui.keyUp('u')
+        pyautogui.keyDown('i')
+        pyautogui.keyUp('i')
+        pyautogui.keyDown('o')
+        pyautogui.keyUp('o')
+
+        pyautogui.keyDown('s')
+
+        pyautogui.keyDown('j')
+        pyautogui.keyUp('j')
+        pyautogui.keyDown('k')
+        pyautogui.keyUp('k')
+        pyautogui.keyDown('l')
+        pyautogui.keyUp('l')
+        pyautogui.keyDown('u')
+        pyautogui.keyUp('u')
+        pyautogui.keyDown('i')
+        pyautogui.keyUp('i')
+        pyautogui.keyDown('o')
+        pyautogui.keyUp('o')
+
+        pyautogui.keyUp('s')
+
+# Exception handle 
+# this section needs work and sometimes fails to function properly
+    except Exception:
+        print ("Exception thrown, calculating course of action")
+        counter += 1
+        print ("counter =" + str(counter))
+        if counter >= 5: counter = 0
+        pyautogui.time.sleep(2)
